9f8a80ef4f9b2c507e20e3e271f18a7dadb89571
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-14 15:03:16 +0100

    made Status file

---
+++ src/components/Status.js
@@ -0,0 +1,11 @@
+const Status = ({applicationsOpen, handleButtonClick}) => {
+
+    return (
+        <>
+            <p>Applications are {applicationsOpen ? "open" : "closed"}. </p>
+            <button onClick={handleButtonClick}>Open/Close Applications</button>
+        </>
+    );
+};
+
+export default Status;
